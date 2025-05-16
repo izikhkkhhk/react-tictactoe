f2483844f5535c1dc5ac179b99baa4d23d22adc5
Bohdan Bura <bohdan.bura@teb.edu.pl>
2025-05-16 09:42:19 +0200

    Zmiany

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-export default function Square() {
-  return <button className="square">x</button>
+import {useState} from 'react';
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
