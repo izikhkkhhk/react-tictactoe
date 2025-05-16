0f450839a070f533a9beac7cda2a2f5ac380b6fe
Bohdan Bura <bohdan.bura@teb.edu.pl>
2025-05-16 09:16:42 +0200

    komicik

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return <button className="square">x</button>
+}
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">x</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+      </div>
+      <div className="board-row">
+        <button className="square">x</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+      </div>
+      <div className="board-row">
+        <button className="square">x</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+      </div>
+    </>
+  );
 }
